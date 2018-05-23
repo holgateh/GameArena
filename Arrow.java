9bb9fffa5d4db258bfcdc5b206cfc58256470182
Joe Finney <joe@comp.lancs.ac.uk>
2018-05-23 09:31:58 +0100

    Fix issue ##1 where body of arrow is not update on setStart() or setEnd().

--- Arrow.java
+++ Arrow.java
@@ -207,6 +207,9 @@ public class Arrow
         ay1 = ye - (ye - ay1) * (arrowHeadLength / 100.0); 
         ay2 = ye - (ye - ay2) * (arrowHeadLength / 100.0); 
 
+        line[0].setStart(xStart, yStart);
+        line[0].setEnd(xEnd, yEnd);
+
         line[1].setStart(ax1, ay1);
         line[1].setEnd(xe, ye);
 
