6f69de88c82a0ff3d8271e739b5fd3861b234599
Joe Finney <joe@comp.lancs.ac.uk>
2019-05-02 16:18:23 +0100

    Add sample / test case for mouse input

---
+++ samples/MouseTest/MouseTest.java
@@ -0,0 +1,34 @@
+public class MouseTest
+{
+	public static void main(String[] args)
+	{
+		GameArena a = new GameArena(300,300);
+		Ball b = new Ball(150,150, 10, "GREEN");
+        int speed = 2;
+
+		a.addBall(b);
+
+		while(true)
+		{
+            if (a.rightPressed())
+			    b.setXPosition(b.getXPosition() + speed);
+
+            if (a.leftPressed())
+			    b.setXPosition(b.getXPosition() - speed);
+
+            if (a.upPressed())
+			    b.setYPosition(b.getYPosition() - speed);
+
+            if (a.downPressed())
+				b.setYPosition(b.getYPosition() + speed);
+			
+			if (a.leftMousePressed() || a.rightMousePressed())
+			{
+				b.setXPosition(a.getMouseX());
+				b.setYPosition(a.getMouseY());
+			}
+
+			a.update();
+		}
+	}
+}
